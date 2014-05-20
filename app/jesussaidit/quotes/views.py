33ee646362de6f188c740ea0b4b97ac424b588ff
Stephen Hawkes <steve@hawkz.com>
2014-05-20 22:43:50 +0100

    Random Jesus'

--- app/jesussaidit/quotes/views.py
+++ app/jesussaidit/quotes/views.py
@@ -1,12 +1,18 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from jesussaidit.quotes.models import Quote, Chapter
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+from random import randint
 
 
 class AllQuotesView(ListView):
     queryset = Quote.objects.select_related().defer("chapter__content").all()
     allow_empty = False
+
+    def get_context_data(self, **kwargs):
+        context = super(AllQuotesView, self).get_context_data(**kwargs)
+        context['img'] = 'img/jesus{r}.png'.format(r=randint(1, 30))
+        return context
 
 
 def quote_view(request, book, chapter, verse):
@@ -18,6 +24,7 @@ def quote_view(request, book, chapter, verse):
 
     return TemplateResponse(request, 'quotes/quote_detail.html', {
         'object': obj,
+        'img': 'img/jesus{r}.png'.format(r=randint(1, 30)),
     })
 
 
@@ -29,4 +36,5 @@ def chapter_view(request, book, chapter):
 
     return TemplateResponse(request, 'quotes/chapter_detail.html', {
         'object': obj,
+        'img': 'img/jesus{r}.png'.format(r=randint(1, 30)),
     })
